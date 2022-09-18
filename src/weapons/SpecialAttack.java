a1953f9b39bebc7148f6646dd21cdbe5308ba3e4
xleo0002 <xleo0002@student.monash.edu>
2022-09-19 00:10:20 +0800

    created SpecialAttack enum class

---
+++ src/weapons/SpecialAttack.java
@@ -0,0 +1,18 @@
+package weapons;
+
+public enum SpecialAttack {
+    EMBER("sparks"),
+    BUBBLE("burbles"),
+    VINEWHIP("whips");
+
+    private final String label;
+
+    SpecialAttack(String label){
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
+}
