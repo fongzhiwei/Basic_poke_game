c632562443ec9ee3f416fddcfcc98fa9ea591c80
xleo0002 <xleo0002@student.monash.edu>
2022-09-19 03:53:17 +0800

    added constructor, doing abstract method

--- src/pokemon/Bulbasaur.java
+++ src/pokemon/Bulbasaur.java
@@ -1,4 +1,48 @@
 package pokemon;
 
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.AttackAction;
+import game.Element;
+import game.behaviours.Behaviour;
+import game.behaviours.WanderBehaviour;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+
 public class Bulbasaur extends Pokemon {
+
+    /**
+     * Constructor.
+     */
+    public Bulbasaur() {
+        super("Bulbasaur", 'b', 100, Element.GRASS);
+        // HINT: add more relevant behaviours here
+        this.addCapability(Element.GRASS);
+        this.behaviours.put(10, new WanderBehaviour());
+    }
+
+    /**
+     * @param otherActor the Actor that might perform an action.
+     * @param direction  String representing the direction of the other Actor
+     * @param map        current GameMap
+     * @return list of game.actions
+     */
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        actions.add(new AttackAction(this, direction));
+        //FIXME: allow other actor to attack this Bulbasaur (incl. Player). Please check requirement! :)
+        return actions;
+    }
 }
