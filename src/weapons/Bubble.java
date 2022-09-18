9c8ad12f8d5876dd6f3dd7d23bda103e316c65a2
xleo0002 <xleo0002@student.monash.edu>
2022-09-19 00:11:43 +0800

    created classes for each weapon (ember, bubble, vinewhip)

---
+++ src/weapons/Bubble.java
@@ -0,0 +1,7 @@
+package weapons;
+
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class Bubble extends WeaponItem{
+}
---
+++ src/weapons/VineWhip.java
@@ -0,0 +1,7 @@
+package weapons;
+
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class VineWhip extends WeaponItem{
+}
