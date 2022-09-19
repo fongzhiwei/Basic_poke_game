54006343f10b4bc74135e331145693ec8cabf652
xleo0002 <xleo0002@student.monash.edu>
2022-09-19 13:35:54 +0800

    FeedAction done

--- src/game/actions/FeedAction.java
+++ src/game/actions/FeedAction.java
@@ -1,4 +1,53 @@
 package game.actions;
 
-public class FeedAction {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+import game.AffectionManager;
+import game.items.Pokefruit;
+import pokemon.Pokemon;
+
+public class FeedAction extends Action {
+    protected Pokemon target;
+
+    public FeedAction(Pokemon target) {
+        this.target = target;
+    }
+
+    public String execute(Actor actor, GameMap map) {
+        if (!this.target.isConscious()) {
+            return this.target + "is unconscious.";
+        }
+        else if (actor.getInventory().size() == 0) {
+            return actor + "'s inventory is empty";
+        }
+        else {
+            int index = 0;
+
+            while (index < actor.getInventory().size()) {
+                if (actor.getInventory().get(index) instanceof Pokefruit) {
+                    Item pokefruit = actor.getInventory().get(index);
+                    actor.removeItemFromInventory(pokefruit);
+
+                    if ((pokefruit.hasCapability((Enum<?>) this.target.capabilitiesList()))) { // not sure
+                        return AffectionManager.getInstance().increaseAffection(this.target, 20);
+                    }
+                    else {
+                        return AffectionManager.getInstance().decreaseAffection(this.target, 10);
+                    }
+                }
+                index += 1;
+            }
+            return actor + " does not have pokefruit";
+        }
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " feeds " + target;
+    }
+
 }
