62967ef5eb8412da9247dc99cf0169f31260c3ce
xleo0002 <xleo0002@student.monash.edu>
2022-09-21 15:32:30 +0800

    commented local variable 'direction'

--- src/game/actions/FeedAction.java
+++ src/game/actions/FeedAction.java
@@ -12,11 +12,11 @@ import game.pokemon.Pokemon;
 
 public class FeedAction extends Action {
     protected Pokemon target;
-    protected String direction;
+//    protected String direction;
 
     public FeedAction(Pokemon target, String direction) {
         this.target = target;
-        this.direction = direction;
+//        this.direction = direction;
     }
 
     public String execute(Actor actor, GameMap map) {
