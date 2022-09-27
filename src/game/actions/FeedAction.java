7223521157523ed33235a5c15dc43196942e1714
SOHMJ <80372585+SOHMJ@users.noreply.github.com>
2022-09-27 21:38:48 +0800

    space

--- src/game/actions/FeedAction.java
+++ src/game/actions/FeedAction.java
@@ -67,7 +67,7 @@ public class FeedAction extends Action {
             return String.format("%s dislikes it! %s.", this.target, AffectionManager.getInstance().decreaseAffection(this.target, 10));
         }
     }
-
+    
     /**
      * Method to return appropriate menu description for the action
      * @param actor the actor who performed the action
