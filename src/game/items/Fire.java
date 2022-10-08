9d56558ed2f0c764ea2354376392f09c87afd843
xleo0002 <xleo0002@student.monash.edu>
2022-10-08 14:50:31 +0800

    created new fire class

---
+++ src/game/items/Fire.java
@@ -0,0 +1,9 @@
+package game.items;
+
+import edu.monash.fit2099.engine.items.Item;
+
+public class Fire extends Item {
+    public Fire() {
+        super("Fire Spin Effect", 'v', false);
+    }
+}
