b7aabb20d384bb59d2ca7dc6c18d2ff58d0f13b0
xleo0002 <xleo0002@student.monash.edu>
2022-10-13 10:36:11 +0800

    modified allowableAction() method

--- src/game/pokemon/Pokemon.java
+++ src/game/pokemon/Pokemon.java
@@ -224,8 +224,9 @@ public abstract class Pokemon extends Actor{
                         actions.add(new CaptureAction(this, direction));
                     }
 
-                    if (this.hasCapability(AffectionLevel.MAX)) {
-                        actions.add(new EvolveAction(this));
+//                    if (this.hasCapability(AffectionLevel.MAX)) {
+                    if (AffectionManager.getInstance().getAffectionPoint(otherActor, this) == AffectionLevel.MAX.getPoints()) {
+                            actions.add(new EvolveAction(this));
                     }
 
                     for (Item elem: otherActor.getInventory()) {
