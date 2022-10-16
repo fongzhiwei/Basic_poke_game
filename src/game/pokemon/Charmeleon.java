8f3024bd19bdb66b038931485f587303a4a5304d
xleo0002 <xleo0002@student.monash.edu>
2022-10-16 15:52:23 +0800

    implemented CanEvolve interface, override playTurn and allowableAction methods

--- src/game/pokemon/Charmeleon.java
+++ src/game/pokemon/Charmeleon.java
@@ -1,17 +1,21 @@
 package game.pokemon;
 
 
+import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.AffectionLevel;
 import game.AffectionManager;
+import game.Character;
 import game.Element;
 import game.Utils;
 import game.actions.EvolveAction;
 import game.behaviours.EvolveBehaviour;
+import game.time.TimePerceptionManager;
 import game.weapons.SpecialWeapon;
 
 /**
@@ -23,7 +27,12 @@ import game.weapons.SpecialWeapon;
  * Modified by:
  *
  */
-public class Charmeleon extends Pokemon {
+public class Charmeleon extends Pokemon implements CanEvolve {
+    /**
+     * The starting turn number when a Pokemon is spawned
+     */
+    private final int birthCount;
+
     /**
      * Constructor.
      */
@@ -31,6 +40,7 @@ public class Charmeleon extends Pokemon {
         super("Charmeleon", 'C', 150);
         this.addCapability(Element.FIRE);
         this.getBehaviours().put(0, new EvolveBehaviour());
+        this.birthCount = TimePerceptionManager.getInstance().getTurn();
     }
 
     /**
@@ -72,6 +82,33 @@ public class Charmeleon extends Pokemon {
     }
 
     /**
+     * Get the turn number for when the Pokemon is alive since it was spawned to the game world
+     *
+     * @return number of turns the Pokemon has survived
+     */
+    @Override
+    public boolean canPokemonEvolve() {
+        return TimePerceptionManager.getInstance().getTurn() - this.birthCount >= 20;
+    }
+
+    /**
+     * Select and return an action to perform on the current turn.
+     *
+     * @param actions    collection of possible Actions for this Actor
+     * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+     * @param map        the map containing the Actor
+     * @param display    the I/O object to which messages may be written
+     * @return the Action to be performed
+     */
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        if (this.canPokemonEvolve()) {
+            this.getBehaviours().put(0, new EvolveBehaviour());
+        }
+        return super.playTurn(actions, lastAction, map, display);
+    }
+
+    /**
      * Returns a new collection of the Actions that the otherActor can do to the current Actor.
      *
      * @param otherActor the Actor that might be performing attack
@@ -83,8 +120,10 @@ public class Charmeleon extends Pokemon {
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
         ActionList actions = super.allowableActions(otherActor, direction, map);
 
-        if (AffectionManager.getInstance().getAffectionPoint(otherActor, this) == AffectionLevel.MAX.getPoints()) {
-            actions.add(new EvolveAction(this));
+        if (otherActor.hasCapability(Character.PLAYER)) {
+            if (AffectionManager.getInstance().getAffectionPoint(otherActor, this) == AffectionLevel.MAX.getPoints()) {
+                actions.add(new EvolveAction(this));
+            }
         }
 
         return actions;
